bf5723140879ca2b2ce7e2df59999839ed7f8e4d
Karson <kars.kalt@gmail.com>
2021-03-15 21:09:27 -0400

    Set up trefle connection API and built plant class

--- app/services/plant.rb
+++ app/services/plant.rb
@@ -1,7 +1,31 @@
 class Plant
+    attr_accessor :id, :common_name, :slug, :scientific_name, :image_url
 
-    def initialize(query)
-        JSON.parse(TrefleConnection.search(query)[0])
+    def initialize
+    end
+
+    def self.new_from_search(query)
+        new_plant = self.new
+        result = TrefleConnection.search(query)[0]
+
+        new_plant.id = result["id"]
+        new_plant.common_name = result["common_name"]
+        new_plant.slug = result["slug"]
+        new_plant.scientific_name = result["scientific_name"]
+        new_plant.image_url = result["image_url"]
+        new_plant
+    end
+
+    def self.find_from_id(id)
+        new_plant = self.new
+        result = TrefleConnection.id(id)
+
+        new_plant.id = result["id"]
+        new_plant.common_name = result["common_name"]
+        new_plant.slug = result["slug"]
+        new_plant.scientific_name = result["scientific_name"]
+        new_plant.image_url = result["image_url"]
+        new_plant
     end
 
 end
--- app/services/trefle_connection.rb
+++ app/services/trefle_connection.rb
@@ -3,13 +3,16 @@ class TrefleConnection
     # https://trefle.io/api/v1/plants/search?token=YOUR_TREFLE_TOKEN&q=coconut
 
     include HTTParty
-    token = 'I5Y1VynV0FjlAP03uX8ZVDfUi1zeTqYiDODOgyBSi-0'
-    base_uri "https://trefle.io/api/v1/plants/search?token=#{token}&q="
-
+    base_uri "https://trefle.io/api/v1/plants"
 
 
     def self.search(query)
-        response = self.get(query.parameterize)
+        response = self.get("/search?token=I5Y1VynV0FjlAP03uX8ZVDfUi1zeTqYiDODOgyBSi-0&q=#{query.parameterize}")
+        response["data"]
+    end
+
+    def self.id(id)
+        response = self.get("/#{id}?token=I5Y1VynV0FjlAP03uX8ZVDfUi1zeTqYiDODOgyBSi-0")
         response["data"]
     end
 
