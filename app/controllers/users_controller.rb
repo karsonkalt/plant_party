52ca77e91faa86e992082eac21354fedae64d292
Karson <kars.kalt@gmail.com>
2021-03-23 18:03:58 -0400

    Added links and worked on all controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,7 @@ class UsersController < ApplicationController
     def index
     end
 
+    def show
+        @user = User.find_by_id(params["id"])
+    end
 end
--- app/models/plant_watering.rb
+++ app/models/plant_watering.rb
@@ -1,3 +1,7 @@
 class PlantWatering < ApplicationRecord
   belongs_to :plant
+
+  def date_of_watering
+    self.created_at.strftime("%B %d, %Y")
+  end
 end
